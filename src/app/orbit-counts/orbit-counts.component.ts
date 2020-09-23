df4f9bee6c899ac6e318b66c647043693414fc4c
Kathy <kathryn.salek@gmail.com>
2020-09-22 20:30:25 -0500

    Updated orbit report with type counts

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  satelliteCounts(satelliteType: string) {
+    let typeCount = 0;
+    for(let i=0; i < this.satellites.length; i++) {
+       if (this.satellites[i].type === satelliteType) {
+         typeCount++;
+       }
+    }
+    return typeCount;
+}
+}
